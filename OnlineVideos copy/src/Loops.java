76ed9c7ba1494a8330ba70eea06cc8e126e4e5f7
Kolla Leela Kishan <kollakishan@users.noreply.github.com>
2020-08-25 16:59:43 -0700

    commit 1

---
+++ OnlineVideos copy/src/Loops.java
@@ -0,0 +1,14 @@
+
+public class Loops {
+	
+	public static void main(String[] args)
+	{
+		int a =1;
+		
+		for(a=1; a<=5;a++)
+		{
+			System.out.println(a);
+		}
+	}
+
+}
---
+++ OnlineVideos copy/src/NestedLoops.java
@@ -0,0 +1,18 @@
+
+public class NestedLoops {
+	public static void main (String[] args)
+	{
+		int i=1;
+		int j=1;
+		
+		for(i=1;i <= 6;i++)
+		{
+			for(j=1; j<=6; j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+
+}
